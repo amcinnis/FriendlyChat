2ead193282fce480eeabe34c9395f8fa56601343
Austin McInnis <austin.mcinnis@icloud.com>
2017-02-02 18:26:50 -0800

    Completed Firebase Code Lab

--- ios-starter/swift-starter/FriendlyChatSwift/FCViewController.swift
+++ ios-starter/swift-starter/FriendlyChatSwift/FCViewController.swift
@@ -59,18 +59,60 @@ class FCViewController: UIViewController, UITableViewDataSource, UITableViewDele
   }
 
   deinit {
+    self.ref.child("messages").removeObserver(withHandle: _refHandle)
   }
 
   func configureDatabase() {
+    ref = FIRDatabase.database().reference()
+    // Listen for new messages in the Firebase database
+    _refHandle = self.ref.child("messages").observe(.childAdded, with: { [weak self] (snapshot) -> Void in
+        guard let strongSelf = self else { return }
+        strongSelf.messages.append(snapshot)
+        strongSelf.clientTable.insertRows(at: [IndexPath(row: strongSelf.messages.count-1, section: 0)], with: .automatic)
+    })
   }
 
   func configureStorage() {
+    let storageUrl = FIRApp.defaultApp()?.options.storageBucket
+    storageRef = FIRStorage.storage().reference(forURL: "gs://" + storageUrl!)
   }
 
   func configureRemoteConfig() {
+    remoteConfig = FIRRemoteConfig.remoteConfig()
+    // Create Remote Config Setting to enable developer mode.
+    // Fetching configs from the server is normally limited to 5 requests per hour.
+    // Enabling developer mode allows many more requests to be made per hour, so developers
+    // can test different config values during development.
+    let remoteConfigSettings = FIRRemoteConfigSettings(developerModeEnabled: true)
+    remoteConfig.configSettings = remoteConfigSettings!
   }
 
   func fetchConfig() {
+    var expirationDuration: Double = 3600
+    // If in developer mode cacheExpiration is set to 0 so each fetch will retrieve values from
+    // the server.
+    if (self.remoteConfig.configSettings.isDeveloperModeEnabled) {
+        expirationDuration = 0
+    }
+    
+    // cacheExpirationSeconds is set to cacheExpiration here, indicating that any previously
+    // fetched and cached config would be considered expired because it would have been fetched
+    // more than cacheExpiration seconds ago. Thus the next fetch would go to the server unless
+    // throttling is in progress. The default expiration duration is 43200 (12 hours).
+    remoteConfig.fetch(withExpirationDuration: expirationDuration) { (status, error) in
+        if (status == .success) {
+            print("Config fetched!")
+            self.remoteConfig.activateFetched()
+            let friendlyMsgLength = self.remoteConfig["friendly_msg_length"]
+            if (friendlyMsgLength.source != .static) {
+                self.msglength = friendlyMsgLength.numberValue!
+                print("Friendly msg length config: \(self.msglength)")
+            }
+        } else {
+            print("Config not fetched")
+            print("Error \(error)")
+        }
+    }
   }
 
   @IBAction func didPressFreshConfig(_ sender: AnyObject) {
@@ -82,13 +124,18 @@ class FCViewController: UIViewController, UITableViewDataSource, UITableViewDele
   }
 
   @IBAction func didPressCrash(_ sender: AnyObject) {
+    FIRCrashMessage("Cause Crash button clicked")
     fatalError()
   }
 
   func logViewLoaded() {
+    FIRCrashMessage("View loaded")
   }
 
   func loadAd() {
+    self.banner.adUnitID = kBannerAdUnitID
+    self.banner.rootViewController = self
+    self.banner.load(GADRequest())
   }
 
   func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
@@ -103,12 +150,36 @@ class FCViewController: UIViewController, UITableViewDataSource, UITableViewDele
     return messages.count
   }
 
-  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-    // Dequeue cell
-    let cell = self.clientTable.dequeueReusableCell(withIdentifier: "tableViewCell", for: indexPath)
-
-    return cell
-  }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        // Dequeue cell
+        let cell = self.clientTable .dequeueReusableCell(withIdentifier: "tableViewCell", for: indexPath)
+        // Unpack message from Firebase DataSnapshot
+        let messageSnapshot: FIRDataSnapshot! = self.messages[indexPath.row]
+        let message = messageSnapshot.value as! Dictionary<String, String>
+        let name = message[Constants.MessageFields.name] as String!
+        if let imageURL = message[Constants.MessageFields.imageURL] {
+            if imageURL.hasPrefix("gs://") {
+                FIRStorage.storage().reference(forURL: imageURL).data(withMaxSize: INT64_MAX){ (data, error) in
+                    if let error = error {
+                        print("Error downloading: \(error)")
+                        return
+                    }
+                    cell.imageView?.image = UIImage.init(data: data!)
+                }
+            } else if let URL = URL(string: imageURL), let data = try? Data(contentsOf: URL) {
+                cell.imageView?.image = UIImage.init(data: data)
+            }
+            cell.textLabel?.text = "sent by: \(name)"
+        } else {
+            let text = message[Constants.MessageFields.text] as String!
+            cell.textLabel?.text = name! + ": " + text!
+            cell.imageView?.image = UIImage(named: "ic_account_circle")
+            if let photoURL = message[Constants.MessageFields.photoURL], let URL = URL(string: photoURL), let data = try? Data(contentsOf: URL) {
+                cell.imageView?.image = UIImage(data: data)
+            }
+        }
+        return cell
+    }
 
   // UITextViewDelegate protocol methods
   func textFieldShouldReturn(_ textField: UITextField) -> Bool {
@@ -126,6 +197,8 @@ class FCViewController: UIViewController, UITableViewDataSource, UITableViewDele
     if let photoURL = AppState.sharedInstance.photoURL {
       mdata[Constants.MessageFields.photoURL] = photoURL.absoluteString
     }
+    //Push data to Firebase Database
+    self.ref.child("messages").childByAutoId().setValue(mdata)
   }
 
   // MARK: - Image Picker
@@ -142,41 +215,61 @@ class FCViewController: UIViewController, UITableViewDataSource, UITableViewDele
     present(picker, animated: true, completion:nil)
   }
 
-  func imagePickerController(_ picker: UIImagePickerController,
-    didFinishPickingMediaWithInfo info: [String : Any]) {
-      picker.dismiss(animated: true, completion:nil)
-    guard let uid = FIRAuth.auth()?.currentUser?.uid else { return }
-
-    // if it's a photo from the library, not an image from the camera
-    if #available(iOS 8.0, *), let referenceURL = info[UIImagePickerControllerReferenceURL] as? URL {
-      let assets = PHAsset.fetchAssets(withALAssetURLs: [referenceURL], options: nil)
-      let asset = assets.firstObject
-      asset?.requestContentEditingInput(with: nil, completionHandler: { (contentEditingInput, info) in
-        let imageFile = contentEditingInput?.fullSizeImageURL
-        let filePath = "\(uid)/\(Int(Date.timeIntervalSinceReferenceDate * 1000))/\((referenceURL as AnyObject).lastPathComponent!)"
-      })
-    } else {
-      guard let image = info[UIImagePickerControllerOriginalImage] as? UIImage else { return }
-      let imageData = UIImageJPEGRepresentation(image, 0.8)
-      guard let uid = FIRAuth.auth()?.currentUser?.uid else { return }
-      let imagePath = "\(uid)/\(Int(Date.timeIntervalSinceReferenceDate * 1000)).jpg"
+    func imagePickerController(_ picker: UIImagePickerController,
+                               didFinishPickingMediaWithInfo info: [String : Any]) {
+        picker.dismiss(animated: true, completion:nil)
+        guard let uid = FIRAuth.auth()?.currentUser?.uid else { return }
+        
+        // if it's a photo from the library, not an image from the camera
+        if #available(iOS 8.0, *), let referenceURL = info[UIImagePickerControllerReferenceURL] {
+            let assets = PHAsset.fetchAssets(withALAssetURLs: [referenceURL as! URL], options: nil)
+            let asset = assets.firstObject
+            asset?.requestContentEditingInput(with: nil, completionHandler: { [weak self] (contentEditingInput, info) in
+                let imageFile = contentEditingInput?.fullSizeImageURL
+                let filePath = "\(uid)/\(Int(Date.timeIntervalSinceReferenceDate * 1000))/\((referenceURL as AnyObject).lastPathComponent!)"
+                guard let strongSelf = self else { return }
+                strongSelf.storageRef.child(filePath)
+                    .putFile(imageFile!, metadata: nil) { (metadata, error) in
+                        if let error = error {
+                            let nsError = error as NSError
+                            print("Error uploading: \(nsError.localizedDescription)")
+                            return
+                        }
+                        strongSelf.sendMessage(withData: [Constants.MessageFields.imageURL: strongSelf.storageRef.child((metadata?.path)!).description])
+                }
+            })
+        } else {
+            guard let image = info[UIImagePickerControllerOriginalImage] as! UIImage? else { return }
+            let imageData = UIImageJPEGRepresentation(image, 0.8)
+            let imagePath = "\(uid)/\(Int(Date.timeIntervalSinceReferenceDate * 1000)).jpg"
+            let metadata = FIRStorageMetadata()
+            metadata.contentType = "image/jpeg"
+            self.storageRef.child(imagePath)
+                .put(imageData!, metadata: metadata) { [weak self] (metadata, error) in
+                    if let error = error {
+                        print("Error uploading: \(error)")
+                        return
+                    }
+                    guard let strongSelf = self else { return }
+                    strongSelf.sendMessage(withData: [Constants.MessageFields.imageURL: strongSelf.storageRef.child((metadata?.path)!).description])
+            }
+        }
     }
-  }
 
   func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
     picker.dismiss(animated: true, completion:nil)
   }
 
-  @IBAction func signOut(_ sender: UIButton) {
-    let firebaseAuth = FIRAuth.auth()
-    do {
-        try firebaseAuth?.signOut()
-        AppState.sharedInstance.signedIn = false
-        dismiss(animated: true, completion: nil)
-    } catch let signOutError as NSError {
-        print ("Error signing out: \(signOutError.localizedDescription)")
+    @IBAction func signOut(_ sender: UIButton) {
+        let firebaseAuth = FIRAuth.auth()
+        do {
+            try firebaseAuth?.signOut()
+            AppState.sharedInstance.signedIn = false
+            dismiss(animated: true, completion: nil)
+        } catch let signOutError as NSError {
+            print ("Error signing out: \(signOutError.localizedDescription)")
+        }
     }
-  }
 
   func showAlert(withTitle title: String, message: String) {
     DispatchQueue.main.async {
